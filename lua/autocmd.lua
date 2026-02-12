99ba7cf1f1e721ee2eb9a82b3e32aff0059612b3
Geoffrey <geoffreyjount@proton.me>
2026-02-12 20:01:14 +0100

    feat: add autocommands

---
+++ lua/autocmd.lua
@@ -0,0 +1,16 @@
+local function format_go_code()
+  vim.lsp.buf.format {
+    filter = function(client)
+      return client.name == "gopls"
+    end,
+
+    bufnr = vim.api.nvim_get_current_buf(),
+  }
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    format_go_code()
+  end,
+})
